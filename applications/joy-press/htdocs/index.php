85ae96099d680514e8b069b420ff01ed2a21f75a
meddah <meddah@meddah.org>
2009-12-11 17:22:57 +0000

    joy-press htdocs

---
+++ applications/joy-press/htdocs/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// generated file by joy script
+define("FRAMEWORK_ROOT", "/opt/joy/framework");
+define("APPLICATION_ROOT", realpath("../"));
+
+require_once(FRAMEWORK_ROOT."/bootstrap.php");
+using("system.web.RequestHandler")->Start();
+
+?>
